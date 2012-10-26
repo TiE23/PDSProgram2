65c6010df79f62796b77ed958e458ebcfa1acfcf
tie23 <kyle.geib@gmail.com>
2012-10-25 17:20:01 -0700

    Playing with mandel stuff. Now starting to write my own UI-free code. Well, nothing yet. But this will be the start.

--- src/Mandel2.java
+++ src/Mandel2.java
@@ -2,17 +2,24 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+
 public final class Mandel2 extends Applet implements MouseListener {
   private int max = 64;
-  private Color[] colors = new Color[50];
+  private Color[] colors = new Color[150];
   private double viewX = 0.0;
   private double viewY = 0.0;
   private double zoom = 1.0;
   private int mouseX;
   private int mouseY;
-
+  
+  private static Frame frame;
+  
 public static void main(String[] args) {
-    Frame frame = new Frame("Fractal Viewer");
+    //Frame 
+    frame = new Frame("Fractal Viewer");
     frame.addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent e) {
         System.exit(0);
@@ -20,7 +27,7 @@ public static void main(String[] args) {
     });
     Applet applet = new Mandel2();
     frame.add(applet, BorderLayout.CENTER);
-    frame.setSize(400, 400);
+    frame.setSize(800, 800);
     frame.setVisible(true);
     applet.init();
     applet.start();
@@ -28,13 +35,18 @@ public static void main(String[] args) {
   
   public void init() {
     addMouseListener(this);
+    
+    // Build a pallet of color.
     for (int i = 0; i < colors.length; i++) {
-	  //int c = 2 * i * 256 / colors.length;
-	  //if (c > 255)
-	  //  c = 511 - c;
-	  int c = ( i / colors.length) * 255;
-		
-	  colors[i] = new Color(c, c, c);
+    	int c = (( i * 2 * 255 ) / colors.length);
+    	if (c > 255)
+    		c = 511 - c;
+	  
+    	int offColor = 0;
+    	if (c > 5)
+    		offColor = (int) (c/1.3);
+	  
+    	colors[i] = new Color( offColor/3, offColor, c );
     }
   }
 
@@ -99,6 +111,22 @@ public static void main(String[] args) {
     }
     repaint();
     System.out.println("Zoom level: " + zoom + ", Max Iterations: " + max );
+    
+    
+    try {
+    	BufferedImage awtImage = 
+    			new BufferedImage(frame.getWidth(),frame.getHeight(),
+    					BufferedImage.TYPE_INT_RGB);
+    	frame.printAll(awtImage.getGraphics());
+    	//Graphics g = awtImage.getGraphics();
+    	//frame.printAll(g);
+    	
+        // Save as JPEG
+        File file = new File("newimage.jpg");
+        ImageIO.write(awtImage, "jpg", file);
+    } catch (IOException err) {
+    }
+    
   }
 
   public void mouseClicked(MouseEvent e) {}
